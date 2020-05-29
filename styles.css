5316b9e940074407effaca8bf6514a906a122040
tanzila-abedin <60758407+tanzila-abedin@users.noreply.github.com>
2020-05-29 14:51:56 +0200

    Update styles.css

--- styles.css
+++ styles.css
@@ -1,1 +1,57 @@
 
+html{
+    text-align: center;
+    font-family: cursive;
+
+}
+
+body{
+background: rgb(214, 210, 210);
+
+
+}
+h1 {
+    border: 0;
+    padding: 0;
+    font-size: 2em;
+    text-align: center;
+    font-family: Arial;
+    font-style: italic;
+    font-weight: bold;
+}
+
+.right {
+    float: center;
+    float: bottom ;
+    flex-wrap: wrap;
+}
+
+.page {
+    display: inline-block;
+    margin: 0px;
+    padding: 0px;
+}
+
+.square {
+    background-color: white;
+    border: 1px solid gray;
+}
+
+.color {
+    background-color: black;
+    border: 1px solid black
+}
+
+.gridContainer {
+    margin-top: 15px;
+    margin-right: 40px;
+    display: inline-grid;
+    grid-template-columns: repeat(16,2fr);
+    grid-template-rows: repeat(16,2fr) ;
+    border: 15px solid rgb(165, 134, 194);
+    border-radius: 5px;
+    height: 500px;      
+    width: 800px;
+}
+
+
